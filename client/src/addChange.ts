936bc72fa2253452feef29e0f0c2fd6d86cf0bc2
Yuki Ueda <ikuyadeu0513@gmail.com>
2021-03-25 13:53:33 +0900

    update devreplay

--- client/src/addChange.ts
+++ client/src/addChange.ts
@@ -6,89 +6,89 @@ import { Rule, makeDiffObj, makeRules } from 'devreplay';
 import { getDiff } from './diffprovider';
 
 export async function addChange() {
-    const config = workspace.getConfiguration('devreplay');
-    let ruleSize = config.get<number>('rule.size');
-    if (ruleSize === undefined) {
-        ruleSize = 1;
-    }
-    const activeTextEditor = window.activeTextEditor;
-    if (activeTextEditor === undefined) {
-        return;
-    }
-    const targetFile = activeTextEditor.document.uri.fsPath;
-    const source = getFileSource(targetFile);
-    const diff = await getDiff(targetFile);
-    const workspaceFolder = workspace.getWorkspaceFolder(activeTextEditor.document.uri);
-    if (workspaceFolder === undefined) {
-        return;
-    }
+	const config = workspace.getConfiguration('devreplay');
+	let ruleSize = config.get<number>('rule.size');
+	if (ruleSize === undefined) {
+		ruleSize = 1;
+	}
+	const activeTextEditor = window.activeTextEditor;
+	if (activeTextEditor === undefined) {
+		return;
+	}
+	const targetFile = activeTextEditor.document.uri.fsPath;
+	const source = getFileSource(targetFile);
+	const diff = await getDiff(targetFile);
+	const workspaceFolder = workspace.getWorkspaceFolder(activeTextEditor.document.uri);
+	if (workspaceFolder === undefined) {
+		return;
+	}
 
-    const rules: Rule[] = [];
-    const chunks = makeDiffObj(diff).filter(chunk => {return chunk.type === 'changed';});
-    for (const out of chunks.filter(chunk => {return chunk.type === 'changed';})) {
-        const pattern = await makeRules(out.deleted.join('\n'),
-                                        out.added.join('\n'), source);
-        if (pattern !== undefined &&
+	const rules: Rule[] = [];
+	const chunks = makeDiffObj(diff).filter(chunk => {return chunk.type === 'changed';});
+	for (const out of chunks.filter(chunk => {return chunk.type === 'changed';})) {
+		const pattern = await makeRules(out.deleted.join('\n'),
+			out.added.join('\n'), source);
+		if (pattern !== undefined &&
             pattern.before.length <= ruleSize &&
             pattern.after.length <= ruleSize) {
-            rules.push(pattern);
-        }
-    }
+			rules.push(pattern);
+		}
+	}
 
-    writePattern(workspaceFolder.uri.fsPath, rules);
+	writePattern(workspaceFolder.uri.fsPath, rules);
 }
 
 
 export function writePattern(rootPath: string, rules: Rule[]) {
-    const outPatterns = readCurrentPattern(rootPath).concat(rules);
-    const patternStr = JSON.stringify(outPatterns, undefined, 2);
-    const filePath = getDevReplayPath(rootPath);
-    try {
-        fs.writeFileSync(filePath, patternStr);
-    } catch(err) {
-        window.showErrorMessage(err.name);
-    }
+	const outPatterns = readCurrentPattern(rootPath).concat(rules);
+	const patternStr = JSON.stringify(outPatterns, undefined, 2);
+	const filePath = getDevReplayPath(rootPath);
+	try {
+		fs.writeFileSync(filePath, patternStr);
+	} catch(err) {
+		window.showErrorMessage(err.name);
+	}
 }
 
 
 function readCurrentPattern(rootPath: string): Rule[] {
-    const devreplayPath = getDevReplayPath(rootPath);
-    if (devreplayPath === undefined) { return []; }
-    let fileContents = undefined;
-    try{
-        fileContents = tryReadFile(devreplayPath);
-    } catch {
-        return [];
-    }
-    if (fileContents === undefined) {
-        return [];
-    }
-    return JSON.parse(fileContents) as Rule[];
+	const devreplayPath = getDevReplayPath(rootPath);
+	if (devreplayPath === undefined) { return []; }
+	let fileContents = undefined;
+	try{
+		fileContents = tryReadFile(devreplayPath);
+	} catch {
+		return [];
+	}
+	if (fileContents === undefined) {
+		return [];
+	}
+	return JSON.parse(fileContents) as Rule[];
 }
 
 
 function getDevReplayPath(rootPath: string) {
-    return join(rootPath, 'devreplay.json');
+	return join(rootPath, 'devreplay.json');
 }
 
 export function tryReadFile(filename: string) {
-    if (!fs.existsSync(filename)) {
-        throw new Error(`Unable to open file: ${filename}`);
-    }
-    const buffer = Buffer.allocUnsafe(256);
-    const fd = fs.openSync(filename, 'r');
-    try {
-        fs.readSync(fd, buffer, 0, 256, 0);
-        if (buffer.readInt8(0) === 0x47 && buffer.readInt8(188) === 0x47) {
-            console.log(`${filename}: ignoring MPEG transport stream\n`);
+	if (!fs.existsSync(filename)) {
+		throw new Error(`Unable to open file: ${filename}`);
+	}
+	const buffer = Buffer.allocUnsafe(256);
+	const fd = fs.openSync(filename, 'r');
+	try {
+		fs.readSync(fd, buffer, 0, 256, 0);
+		if (buffer.readInt8(0) === 0x47 && buffer.readInt8(188) === 0x47) {
+			console.log(`${filename}: ignoring MPEG transport stream\n`);
 
-            return undefined;
-        }
-    } finally {
-        fs.closeSync(fd);
-    }
+			return undefined;
+		}
+	} finally {
+		fs.closeSync(fd);
+	}
 
-    return fs.readFileSync(filename, 'utf8');
+	return fs.readFileSync(filename, 'utf8');
 }
 
 
@@ -97,31 +97,31 @@ interface IGrammarPath {
 }
 
 export const grammarPaths: IGrammarPath = {
-    'source.c': ['.c'],
-    'source.cpp': ['.cpp'],
-    'source.csharp': ['.cs'],
-    'source.go': ['.go'],
-    'source.html': ['.html'],
-    'source.java': ['.java'],
-    'source.js': ['.js'],
-    'source.perl': ['.perl'],
-    'source.perl.6': ['.perl6'],
-    'source.php': ['.php'],
-    'source.python': ['.py'],
-    'source.r': ['.r'],
-    'source.ruby': ['.ruby'],
-    'source.rust': ['.rs'],
-    'source.swift': ['.swift'],
-    'source.ts': ['.ts']
+	'source.c': ['.c'],
+	'source.cpp': ['.cpp'],
+	'source.csharp': ['.cs'],
+	'source.go': ['.go'],
+	'source.html': ['.html'],
+	'source.java': ['.java'],
+	'source.js': ['.js'],
+	'source.perl': ['.perl'],
+	'source.perl.6': ['.perl6'],
+	'source.php': ['.php'],
+	'source.python': ['.py'],
+	'source.r': ['.r'],
+	'source.ruby': ['.ruby'],
+	'source.rust': ['.rs'],
+	'source.swift': ['.swift'],
+	'source.ts': ['.ts']
 };
 
 export function getFileSource(path: string) {
-    for (const grammarPath in grammarPaths) {
-        for (const extension of grammarPaths[grammarPath]) {
-            if (path.endsWith(extension)){
-                return grammarPath;
-            }
-        }
-    }
-    return undefined;
+	for (const grammarPath in grammarPaths) {
+		for (const extension of grammarPaths[grammarPath]) {
+			if (path.endsWith(extension)){
+				return grammarPath;
+			}
+		}
+	}
+	return undefined;
 }
