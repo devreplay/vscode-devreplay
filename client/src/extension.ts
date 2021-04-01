6a55f33dc679d0bbaa3a18fae64a86d90f4b5973
Yuki Ueda <ikuyadeu0513@gmail.com>
2021-04-01 14:42:52 +0900

    refactoring

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,115 +1,124 @@
 'use strict';
 import { join } from 'path';
 
-import { ExtensionContext, window, commands } from 'vscode';
+import { ExtensionContext, window, commands, workspace } from 'vscode';
 import { LanguageClient, LanguageClientOptions, RevealOutputChannelOn, ServerOptions, TransportKind } from 'vscode-languageclient/node';
 import { addChange } from './addChange';
 
 export function activate(context: ExtensionContext): void {
-    const serverModule = context.asAbsolutePath(join('server', 'out', 'server.js'));
-    const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'], cwd: process.cwd() };
-    const serverOptions: ServerOptions = {
-        run: { module: serverModule, transport: TransportKind.ipc, options: { cwd: process.cwd() } },
-        debug: {
-            module: serverModule,
-            transport: TransportKind.ipc,
-            options: debugOptions,
-        },
-    };
-    const clientOptions: LanguageClientOptions = {
-        documentSelector: [
-            {
-                scheme: 'file',
-                language: 'plaintext',
-            },
-            {
-                scheme: 'file',
-                language: 'python',
-            },
-            {
-                scheme: 'file',
-                language: 'php',
-            },
-            {
-                scheme: 'file',
-                language: 'ruby',
-            },
-            {
-                scheme: 'file',
-                language: 'r',
-            },
-            {
-                scheme: 'file',
-                language: 'java',
-            },
-            {
-                scheme: 'file',
-                language: 'javascript',
-            },
-            {
-                scheme: 'file',
-                language: 'typescript',
-            },
-            {
-                scheme: 'file',
-                language: 'cpp',
-            },
-            {
-                scheme: 'file',
-                language: 'c',
-            },
-            {
-                scheme: 'file',
-                language: 'cobol',
-            },
-            {
-                scheme: 'file',
-                language: 'dart',
-            },
-            {
-                scheme: 'file',
-                language: 'go',
-            },
-            {
-                scheme: 'file',
-                language: 'rust',
-            },
-            {
-                scheme: 'file',
-                language: 'csharp',
-            },
-            {
-                scheme: 'file',
-                language: 'kotlin',
-            }
-        ],
-        diagnosticCollectionName: 'devreplay',
-        revealOutputChannelOn: RevealOutputChannelOn.Never,
-    };
+	const serverModule = context.asAbsolutePath(join('server', 'out', 'server.js'));
+	const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'], cwd: process.cwd() };
+	const serverOptions: ServerOptions = {
+		run: { module: serverModule, transport: TransportKind.ipc, options: { cwd: process.cwd() } },
+		debug: {
+			module: serverModule,
+			transport: TransportKind.ipc,
+			options: debugOptions,
+		},
+	};
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: [
+			{
+				scheme: 'file',
+				language: 'plaintext',
+			},
+			{
+				scheme: 'file',
+				language: 'python',
+			},
+			{
+				scheme: 'file',
+				language: 'php',
+			},
+			{
+				scheme: 'file',
+				language: 'ruby',
+			},
+			{
+				scheme: 'file',
+				language: 'r',
+			},
+			{
+				scheme: 'file',
+				language: 'java',
+			},
+			{
+				scheme: 'file',
+				language: 'javascript',
+			},
+			{
+				scheme: 'file',
+				language: 'typescript',
+			},
+			{
+				scheme: 'file',
+				language: 'cpp',
+			},
+			{
+				scheme: 'file',
+				language: 'c',
+			},
+			{
+				scheme: 'file',
+				language: 'cobol',
+			},
+			{
+				scheme: 'file',
+				language: 'dart',
+			},
+			{
+				scheme: 'file',
+				language: 'go',
+			},
+			{
+				scheme: 'file',
+				language: 'rust',
+			},
+			{
+				scheme: 'file',
+				language: 'csharp',
+			},
+			{
+				scheme: 'file',
+				language: 'kotlin',
+			}
+		],
+		synchronize: {
+			// configurationSection: 'devreplay',
+			fileEvents: [
+				workspace.createFileSystemWatcher('**/devreplay.json')
+			]
+		},
+		diagnosticCollectionName: 'devreplay',
+		revealOutputChannelOn: RevealOutputChannelOn.Never,
+		progressOnInitialization: true
+	};
 
-    let client: LanguageClient;
-    try {
-        client = new LanguageClient('DevReplay Server', serverOptions, clientOptions);
-    } catch (err) {
-        window.showErrorMessage('The extension couldn\'t be started. See the output channel for details.');
+	let client: LanguageClient;
+	try {
+		client = new LanguageClient('DevReplay Server', serverOptions, clientOptions);
+	} catch (err) {
+		window.showErrorMessage('The extension couldn\'t be started. See the output channel for details.');
 
-        return;
-    }
-    client.registerProposedFeatures();
+		return;
+	}
+	client.registerProposedFeatures();
 
-    // const config = workspace.getConfiguration('devreplay');
+	// const config = workspace.getConfiguration('devreplay');
 
-    const registeredCommand = commands.registerCommand('devreplay.add', () => { addChange(); } );
-    // workspace.onWillSaveTextDocument((_event) => {
-    //     const isExecutable = config.get<boolean>('exec.save');
-    //     if (isExecutable) {
-    //         addChange();
-    //     }
-    // });
+	const registeredCommand = commands.registerCommand('devreplay.add', () => { addChange(); } );
+	// workspace.onWillSaveTextDocument((_event) => {
+	//     const isExecutable = config.get<boolean>('exec.save');
+	//     if (isExecutable) {
+	//         addChange();
+	//     }
+	// });
 
-    context.subscriptions.push(
-        client.start(),
-        registeredCommand
-    );
+	context.subscriptions.push(
+		client.start(),
+		registeredCommand
+	);
 }
 
+export function deactivate() {
+}
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 a = 1
 b = 2
-print("hello world")
+print("Hello world")
 
 tmp = a
 a = b
